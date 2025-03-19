414b8ef4f9860c3db5bb12237743710660244c57
Hoang Dang <118740300+Pandadanh@users.noreply.github.com>
2025-03-19 17:28:17 +0700

    Update configuration for new repository

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PersonalFinancialManagement/ui-report/fe/',
+  base: '/ui-report-fe/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
